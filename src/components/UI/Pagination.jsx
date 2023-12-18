d4c61b17f8ce2ac4991e77507699700d64fb0cff
UriMakarov <umakarov@colvir.com>
2023-12-18 17:20:59 +0300

    fixes

--- src/components/UI/Pagination.jsx
+++ src/components/UI/Pagination.jsx
@@ -1,8 +1,13 @@
+import firstIcon from "../../assets/Pagination/first.svg"
+import nextIcon from "../../assets/Pagination/next.svg"
+import prevIcon from "../../assets/Pagination/prev.svg"
+import lastIcon from "../../assets/Pagination/last.svg"
+
 export const Pagination = () => {
   return (
     <div className="flex flex-row justify-between items-center">
       <div className="flex flex-row items-center">
-        <p className="mr-[8px]">Объектов на странице</p>
+        <p className="ml-[24px] mr-[8px]">Объектов на странице</p>
         <select
           name="selectedCount"
           defaultValue="100"
@@ -11,6 +16,17 @@ export const Pagination = () => {
           <option value="10">10</option>
           <option value="100">100</option>
         </select>
+      </div>
+
+      <div className="flex flex-row justify-center items-center mr-[32px]">
+        <div className="mr-[22px]"> <img src={firstIcon} alt="firstIcon" /> </div>
+        <div className="mr-[22px]"> <img src={prevIcon} alt="prevIcon" />   </div>
+
+        <div className="w-[32px] h-[32px] rounded-[8px] bg-blue mr-[22px] flex justify-center items-center text-white font-roboto font-bold"> <p>1</p> </div>
+
+        <div className="mr-[22px]"> <img src={nextIcon} alt="nextIcon" /> </div>
+        <div className="mr-[22px]"> <img src={lastIcon} alt="lastIcon" /> </div>
+
       </div>
     </div>
   );
